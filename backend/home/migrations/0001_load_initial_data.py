03c0a26170612db70fca129d3143e4e57ef37dcc
Crowdbotics <engineering@crowdbotics.com>
2022-12-20 20:44:32 +0000

    Initial commit.

---
+++ backend/home/migrations/0001_load_initial_data.py
@@ -0,0 +1,25 @@
+from django.db import migrations
+
+
+def create_site(apps, schema_editor):
+    Site = apps.get_model("sites", "Site")
+    custom_domain = "testauto192-dev-51666.botics.co"
+
+    site_params = {
+        "name": "TestAuto192",
+    }
+    if custom_domain:
+        site_params["domain"] = custom_domain
+
+    Site.objects.update_or_create(defaults=site_params, id=1)
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("sites", "0002_alter_domain_unique"),
+    ]
+
+    operations = [
+        migrations.RunPython(create_site),
+    ]
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "testauto192-dev-51666",
+  url: "https://testauto192-dev-51666.botics.co",
+  api: "https://testauto192-dev-51666.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
